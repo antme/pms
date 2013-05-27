e4b3d2fa0fe9e6762a57aa6363a89f03fe980034
dylan <chenlieping@gmail.com>
2013-05-27 16:07:34 +0800

    UI Structure Refine

--- web/js/ui.js
+++ web/js/ui.js
@@ -7,12 +7,12 @@ $(document).ready(function() {
 	console.log(urlStr);
 	page = jQuery.url.setUrl(urlStr).attr("anchor");
 	if (!page) {
-		page = "local-data.html";
+		page = "html/local-data.html";
 	}
 	loadPage(page);
 	
 	$("#user-m").click(function(){
-		loadPage("userman.html");
+		loadPage("html/user/userman.html");
 	});
 });
 
@@ -32,4 +32,14 @@ function loadPage(page) {
 
 }
 
+
+function onLeftNavSelect(e){
+	var text = this.text(e.node);
+	
+	if(text == "权限管理"){
+		loadPage("html/user/userman.html");
+	}else{
+		loadPage("html/local-data.html");
+	}
+}
 
