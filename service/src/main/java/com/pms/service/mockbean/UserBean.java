f528b943d564e4e6424441cc05965677dc6061b9
dylan <chenlieping@gmail.com>
2013-05-31 16:04:36 +0800

    用户权限修改功能

--- service/src/main/java/com/pms/service/mockbean/UserBean.java
+++ service/src/main/java/com/pms/service/mockbean/UserBean.java
@@ -4,4 +4,7 @@ public class UserBean {
     public static final String USER_NAME = "userName";
     public static final String PASSWORD = "password";
     public static final String ROLES = "roles";
+    public static final String GROUPS = "groups";
+    
+    public static final String GROUPS_DISPLAY = "groupsDisplay";
 }
