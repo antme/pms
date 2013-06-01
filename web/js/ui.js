674bf39aedfc02742e3dc9865de88faa3a7217a6
chenlieping <lieping.chen@qingcheng.com>
2013-06-01 11:24:21 +0800

    solve the page refresh problems by add url anchor

--- web/js/ui.js
+++ web/js/ui.js
@@ -1,20 +1,19 @@
-var roles;
 var page;
-var isLogin = false;
 
 $(document).ready(function() {
 	var urlStr = window.document.location.href;
 	page = jQuery.url.setUrl(urlStr).attr("anchor");
+	console.log(page);
 	if (!page) {
 		page = "html/local-data.html";
 	}
 	loadPage(page);
 
 	$("#user-m").click(function() {
-		loadPage("html/project/project.html");
+		loadPage("userman");
 	});
 	$("#user-info").click(function() {
-		loadPage("local-data.html");
+		loadPage("default");
 	});
 });
 
@@ -30,21 +29,38 @@ function displayMsg(result) {
 }
 
 function loadPage(page, divID) {
-	
-	if(!divID){
+
+	if (!divID) {
 		divID = "#main_right";
 	}
+
 	var uid = kendo.guid();
-	var url = page + "?_uid=" + uid
-	
-	if(page.indexOf("?") != -1){
-		url = page + "&_uid=" + uid
-		
+
+	if (page == "userman") {
+		page = "html/user/userman.html";
+	} else if (page == "project") {
+		page = "html/project/project.html";
+	} else if (page == "group") {
+		page = "html/user/group.html";
+	} else if (page == "customer") {
+		page = "html/customer/customer.html";
+	} else if (page == "supplier") {
+		page = "html/supplier/supplier.html";
+	} else {
+		page = "html/local-data.html";
+	}
+
+	var url = page + "?_uid=" + uid;
+
+	if (page.indexOf("?") != -1) {
+		url = page + "&_uid=" + uid;
+
 	}
 	$.ajax({
 		url : url,
 		success : function(data) {
 			$(divID).html(data);
+
 		},
 		error : onAjaxFail
 	});
@@ -52,26 +68,27 @@ function loadPage(page, divID) {
 }
 
 function onLeftNavSelect(e) {
+	console.log(e);
 	var text = this.text(e.node);
 
-	if (text == "权限管理" || text =="用户管理") {
-		loadPage("html/user/userman.html");
-	}else if (text == "项目管理"){
-		loadPage("html/project/project.html");
-	}else if (text == "角色管理") {
-		loadPage("html/user/group.html");
-	}else if (text == "客户管理") {
-		loadPage("html/customer/customer.html");
-	}else if (text == "供应商") {
-		loadPage("html/supplier/supplier.html");
-	}else{
-		loadPage("html/local-data.html");
+	if (text == "权限管理" || text == "用户管理") {
+		loadPage("userman");
+	} else if (text == "项目管理") {
+		loadPage("project");
+	} else if (text == "角色管理") {
+		loadPage("group");
+	} else if (text == "客户管理") {
+		loadPage("customer");
+	} else if (text == "供应商") {
+		loadPage("supplier");
+	} else {
+		loadPage("default");
 	}
+
 }
 
-function getSelectedRowDataByGrid(gridId){
-	var grid = $("#"+gridId).data("kendoGrid");
+function getSelectedRowDataByGrid(gridId) {
+	var grid = $("#" + gridId).data("kendoGrid");
 	var row = grid.select();
 	return grid.dataItem(row);
 }
-
