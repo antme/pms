3e10e0c429dc6a1938b58b71241e0b2f846319d7
chenlieping <lieping.chen@qingcheng.com>
2013-06-02 16:00:33 +0800

    define ApiRoleValidException

---
+++ service/src/main/java/com/pms/service/exception/ApiRoleValidException.java
@@ -0,0 +1,16 @@
+package com.pms.service.exception;
+
+public class ApiRoleValidException extends RuntimeException{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    
+    
+    public ApiRoleValidException() {
+        super();
+    }
+
+
+}
