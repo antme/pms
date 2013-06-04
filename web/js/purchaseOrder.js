337fd8719d6ee7474644531a89e1c2b262ecb213
dylan <chenlieping@gmail.com>
2013-06-04 16:21:26 +0800

    采购订单代码初始化

---
+++ web/js/purchaseOrder.js
@@ -0,0 +1,131 @@
+var model = kendo.data.Model.define({
+	id : "_id",
+	dataSource : dataSource,
+	fields : {
+		_id : {
+			editable : false,
+			nullable : true
+		},
+		supplierName : {
+			validation : {
+				required : true
+			}
+		},
+		supplierAddress : {
+			validation : {
+				required : true,
+				min : 1
+			}
+		},
+		supplierContact : {},
+		supplierContactPhone : {
+			validation : {
+				min : 0,
+				required : true
+			}
+		}
+	}
+
+});
+var dataSource = new kendo.data.DataSource({
+	transport : {
+		read : {
+			url : "/service/purcontract/list",
+			dataType : "jsonp"
+		},
+		update : {
+			url : "/service/purcontract/update",
+			dataType : "jsonp",
+			type : "post"
+		},
+		create : {
+			url : "/service/purcontract/add",
+			dataType : "jsonp",
+			type : "post"
+		},
+
+		destroy : {
+			url : "/service/purcontract/delete",
+			dataType : "jsonp",
+			type : "post"
+		},
+
+		parameterMap : function(options, operation) {
+			if (operation !== "read" && options.models) {
+				return {
+					models : kendo.stringify(options.models)
+				};
+			}
+		},
+	},
+	pageSize : 20,
+	batch : true,
+	schema : {
+		model : model
+	}
+});
+
+
+
+
+$(document).ready(function() {
+
+	$("#grid").kendoGrid({
+		dataSource : dataSource,
+		pageable : true,
+		editable : "popup",
+		toolbar : [ {
+			name : "create",
+			text : "添加"
+		} ],
+		columns : [ {
+			field : "contractId",
+			title : "采购合同编号"
+		}, {
+			field : "customerContractId",
+			title : "客户合同编号"
+		}, {
+			field : "contractRequestId",
+			title : "采购申请编号"
+		}, {
+			field : "conractOrderId",
+			title : "采购订单编号"
+
+		}, {
+			field : "customerName",
+			title : "客户名"
+		}, {
+			field : "projectManager",
+			title : "PM"
+		}, {
+			field : "status",
+			title : "采购合同状态"
+		}, {
+			field : "contractDate",
+			title : "合同签署时间"
+		}, {
+			field : "contractDate",
+			title : "金额"
+		}, {
+			field : "type",
+			title : "物流类型"
+		}, {
+			field : "supplierName",
+			title : "供应商名"
+		}, {
+			field : "supplierId",
+			title : "供应商编号"
+		},
+
+		{
+			command : [ "edit", {
+				name : "destroy",
+				title : "删除",
+				text : "删除"
+			} ],
+			title : "&nbsp;",
+			width : "160px"
+		} ]
+
+	});
+});
