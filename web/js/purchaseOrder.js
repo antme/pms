93f12604086b7df94dde67aa70b9d7283fa9235d
chenlieping <lieping.chen@qingcheng.com>
2013-06-06 22:15:51 +0800

    采购订单 合同页面

--- web/js/purchaseOrder.js
+++ web/js/purchaseOrder.js
@@ -1,26 +1,26 @@
 //抽象model对象， datasource对象必须绑定一个model为了方便解析parameterMap中需要提交的参数
 var model = kendo.data.Model.define({
 	id : "_id",
-	fields: {
-		
-		totalInContract: {
-			type: "number"
+	fields : {
+
+		totalInContract : {
+			type : "number"
 		},
-		
-		availableAmount : { 
-			type: "number"
+
+		availableAmount : {
+			type : "number"
 		},
-		
+
 		requestedAmount : {
-			type: "number"
+			type : "number"
 		},
 		referenceUnitPrice : {
-			type: "number"
+			type : "number"
 		}
 	}
 });
 
-//外面列表页的datasource对象
+// 外面列表页的datasource对象
 var dataSource = new kendo.data.DataSource({
 	transport : {
 		read : {
@@ -53,7 +53,7 @@ var dataSource = new kendo.data.DataSource({
 	pageSize : 20,
 	batch : true,
 
-	//必须绑定一个model为了方便解析parameterMap中需要提交的参数
+	// 必须绑定一个model为了方便解析parameterMap中需要提交的参数
 	schema : {
 		model : model
 	}
@@ -61,14 +61,14 @@ var dataSource = new kendo.data.DataSource({
 
 $(document).ready(function() {
 
-	//初始化采购订单列表页
+	// 初始化采购订单列表页
 	$("#grid").kendoGrid({
 		dataSource : dataSource,
 		pageable : true,
 		editable : "popup",
-		selectable : "multiple",
-		width: "1000px",
-		//自定义toolbar，参见html中模板代码
+		selectable : "row",
+		width : "1000px",
+		// 自定义toolbar，参见html中模板代码
 		toolbar : [ {
 			template : kendo.template($("#template").html())
 		} ],
@@ -92,7 +92,7 @@ $(document).ready(function() {
 			field : "projectManager",
 			title : "PM"
 		}, {
-			field : "status",
+			field : "stauts",
 			title : "订单状态"
 		}, {
 			field : "approvedDate",
@@ -101,7 +101,7 @@ $(document).ready(function() {
 			field : "totalMoney",
 			title : "金额"
 		}, {
-			field : "numbers",
+			field : "requestedNumbers",
 			title : "合同下采购申请单数量"
 		}, {
 			field : "numbersExists",
@@ -114,7 +114,7 @@ $(document).ready(function() {
 		{
 			command : [ {
 				text : "Edit",
-				//自定义点击事件
+				// 自定义点击事件
 				click : edit
 			}, {
 				name : "destroy",
@@ -128,13 +128,13 @@ $(document).ready(function() {
 	});
 });
 
-//声明一个总的对象用来传递数据
+// 声明一个总的对象用来传递数据
 var requestDataItem;
 
-//当window弹出的时候
+// 当window弹出的时候
 function onRequestSelectWindowActive(e) {
 
-	//获取采购申请的数据，数据包含了成本清单
+	// 获取采购申请的数据，数据包含了成本清单
 	$("#purchaseRequest").kendoDropDownList({
 		dataTextField : "projectName",
 		dataValueField : "_id",
@@ -147,14 +147,14 @@ function onRequestSelectWindowActive(e) {
 			}
 		},
 
-		//当用户选择不同的采购申请时候赋值给requestDataItem对象
+		// 当用户选择不同的采购申请时候赋值给requestDataItem对象
 		select : function(e) {
 			requestDataItem = this.dataSource.at(e.item.index());
 		}
 	});
 }
 
-//窗口属性设置
+// 窗口属性设置
 var reoptions = {
 	id : "purchaseorder-edit",
 	width : "1050px",
@@ -164,6 +164,8 @@ var reoptions = {
 };
 
 function add() {
+	$("#purchase-request-select").show();
+	$("#purchaseorder-edit-item").hide();
 	openWindow(reoptions);
 }
 
@@ -183,7 +185,7 @@ var itemDataSource = new kendo.data.DataSource({
 			if (operation !== "read" && options.models) {
 				return {
 
-					//解析成json_p模式
+					// 解析成json_p模式
 					json_p : kendo.stringify(requestDataItem),
 					mycallback : "checkStatus"
 				};
@@ -191,7 +193,6 @@ var itemDataSource = new kendo.data.DataSource({
 		},
 		requestEnd : function(e) {
 			var response = e.response;
-			console.log(e.type);
 		}
 	},
 	schema : {
@@ -206,75 +207,135 @@ function checkStatus(data) {
 		requestDataItem.set("_id", data._id);
 	}
 }
-//计算成本数据的datasouce
+// 计算成本数据的datasouce
 var sumDataSource = new kendo.data.DataSource({
 
 });
 
 function showOrderWindow() {
 
-	//如果用户用默认的采购申请，select event不会触发， 需要初始化数据
+	// 如果用户用默认的采购申请，select event不会触发， 需要初始化数据
 	var kendoGrid = $("#purchaseRequest").data("kendoDropDownList");
 	if (!requestDataItem) {
 		requestDataItem = kendoGrid.dataSource.at(0);
 	}
 
-	//新增，所以设置_id为空
+	// 新增，所以设置_id为空
 	requestDataItem.set("_id", "");
 	edit();
 }
 
 function submitOrder() {
-	//同步数据
+	// 同步数据
 	itemDataSource.sync();
 }
 
 function sumOrders(e) {
+
 	var data = itemDataSource.data();
 	requestDataItem.orderList = data;
 
-	
-	var referenceUnitPrice =e.model.referenceUnitPrice;
+	var referenceUnitPrice = e.model.referenceUnitPrice;
 	var requestedAmount = e.model.requestedAmount;
-	
-	if(e.values.referenceUnitPrice ){
+
+	if (e.values.referenceUnitPrice) {
 		referenceUnitPrice = e.values.referenceUnitPrice
 	}
-	
-	if(e.values.requestedAmount ){
+
+	if (e.values.requestedAmount) {
 		requestedAmount = e.values.requestedAmount
 	}
-	
 	e.model.set("totalMoney", referenceUnitPrice * requestedAmount);
 
 	var grid1 = $("#purchaseorder-edit-grid").data("kendoGrid");
+	// will trigger dataBound event
 	grid1.refresh();
-	
-	
+}
+
+function approve() {
+	var row = getSelectedRowDataByGrid("grid");
+	if (!row) {
+		alert("点击列表可以选中数据");
+	} else {
+		$.ajax({
+			url : "/service/purcontract/order/approve",
+			success : function(responsetxt) {
+				var res;
+				eval("res=" + responsetxt);
+				if (res.status == "0") {
+					alert(res.msg);
+				} else {
+					alert("审核成功");
+					dataSource.read();
+				}
+			},
+
+			error : function() {
+				alert("连接Service失败");
+			},
+
+			data : {
+				_id : row._id
+			},
+			method : "post"
+		});
+	}
+
+}
+
+function reject() {
+	var row = getSelectedRowDataByGrid("grid");
+
+	if (!row) {
+		alert("点击列表可以选中数据");
+	} else {
+		$.ajax({
+			url : "/service/purcontract/order/reject",
+			success : function(responsetxt) {
+				var res;
+				eval("res=" + responsetxt);
+				if (res.status == "0") {
+					alert(res.msg);
+				} else {
+					alert("拒绝成功");
+					dataSource.read();
+				}
+			},
+
+			error : function() {
+				alert("连接Service失败");
+			},
+
+			data : {
+				_id : row._id
+			},
+			method : "post"
+		});
+	}
 }
 
 function edit(e) {
 
-	//初始化空对象
+	// 初始化空对象
 	var dataItem = new model();
 
 	if (requestDataItem) {
-		//如果是从采购申请选择过来的
+		// 如果是从采购申请选择过来的
 		dataItem = requestDataItem;
 	}
 
 	if (e) {
-		//如果是从订单列表页点击edit过来的数据
+		// 如果是从订单列表页点击edit过来的数据
 		e.preventDefault();
 		openWindow(reoptions);
 		dataItem = this.dataItem($(e.currentTarget).closest("tr"));
 		requestDataItem = dataItem;
-		
-		//隐藏选择采购申请
+
+		// 隐藏选择采购申请
 		$("#purchase-request-select").hide();
 	}
 
-	//渲染成本编辑列表
+	// 渲染成本编辑列表
 	itemDataSource.data(dataItem.orderList);
 
 	$("#orderCode").html(dataItem.orderCode);
@@ -288,110 +349,117 @@ function edit(e) {
 	var editKendoGrid = $("#purchaseorder-sum-grid").data("kendoGrid");
 
 	if (!editKendoGrid) {
-		$("#purchaseorder-edit-grid").kendoGrid({
-			dataSource : itemDataSource,
-			columns : [ {
-				field : "goodsCode",
-				title : "货品编号"
-			}, {
-				field : "goodsName",
-				title : "货品名"
-			}, {
-				field : "goodsType",
-				title : "货品类别"
-			}, {
-				field : "goodsModel",
-				title : "货品型号"
+		$("#purchaseorder-edit-grid")
+				.kendoGrid(
+						{
+							dataSource : itemDataSource,
+							columns : [ {
+								field : "goodsCode",
+								title : "货品编号"
+							}, {
+								field : "goodsName",
+								title : "货品名"
+							}, {
+								field : "goodsType",
+								title : "货品类别"
+							}, {
+								field : "goodsModel",
+								title : "货品型号"
 
-			}, {
-				field : "totalInContract",
-				title : "合同中总数"
-			}, {
-				field : "availableAmount",
-				title : "可申请数量"
-			}, {
-				field : "requestedAmount",
-				title : "本次申请数量"
-			}, {
-				field : "referenceUnitPrice",
-				title : "参考单价"
-			}, {
-				field : "totalMoney",
-				title : "小计金额"
-			}, {
-				field : "orderGoodsCode",
-				title : "订单货品编号"
-			}, {
-				field : "orderGoodsName",
-				title : "订单货品名"
-			}, {
-				field : "orderGoodsModel",
-				title : "订单货品型号"
-			}, {
-				field : "orderGoodsUnitPrice",
-				title : "订单货品单价"
-			}, {
-				field : "orderGoodsTotalMoney",
-				title : "订单实际小计金额"
-			}, {
-				field : "differenceAmount",
-				title : "金额差值"
-			} ],
+							}, {
+								field : "totalInContract",
+								title : "合同中总数"
+							}, {
+								field : "availableAmount",
+								title : "可申请数量"
+							}, {
+								field : "requestedAmount",
+								title : "本次申请数量"
+							}, {
+								field : "referenceUnitPrice",
+								title : "参考单价"
+							}, {
+								field : "totalMoney",
+								title : "小计金额"
+							}, {
+								field : "orderGoodsCode",
+								title : "订单货品编号"
+							}, {
+								field : "orderGoodsName",
+								title : "订单货品名"
+							}, {
+								field : "orderGoodsModel",
+								title : "订单货品型号"
+							}, {
+								field : "orderGoodsUnitPrice",
+								title : "订单货品单价"
+							}, {
+								field : "orderGoodsTotalMoney",
+								title : "订单实际小计金额"
+							}, {
+								field : "differenceAmount",
+								title : "金额差值"
+							} ],
 
-			toolbar : [ "create" ],
-			editable : true,
-			scrollable : true,
-			width : "950px",
-			save : sumOrders,
-			dataBound: function(e) {
-				var kendoGrid = $("#purchaseorder-sum-grid").data("kendoGrid");
-				if(!kendoGrid){
-					$("#purchaseorder-sum-grid").kendoGrid({
-						columns : [ {
-							field : "requestedMoney",
-							title : "申请金额"
-						}, {
-							field : "requestedNumbers",
-							title : "货品数量"
-						}, {
-							field : "numbersPercentOfContract",
-							title : "货品占合同%"
-						}, {
-							field : "moneyPercentOfContract",
-							title : "货品金额占合同%"
+							toolbar : [ "create" ],
+							editable : true,
+							scrollable : true,
+							width : "950px",
+							save : sumOrders,
+							dataBound : function(e) {
+								var kendoGrid = $("#purchaseorder-sum-grid")
+										.data("kendoGrid");
+								if (!kendoGrid) {
+									$("#purchaseorder-sum-grid").kendoGrid({
+										columns : [ {
+											field : "requestedMoney",
+											title : "申请金额"
+										}, {
+											field : "requestedNumbers",
+											title : "货品数量"
+										}, {
+											field : "numbersPercentOfContract",
+											title : "货品占合同%"
+										}, {
+											field : "moneyPercentOfContract",
+											title : "货品金额占合同%"
 
-						} ],
-						width : "200px"
+										} ],
+										width : "200px"
 
-					});
-				}
-				kendoGrid = $("#purchaseorder-sum-grid").data("kendoGrid");
-				var data = itemDataSource.data();
-				var total = 0;
-				var totalMoney = 0;
-				for(i=0; i<data.length; i++){
-					var item = data[i];
-					console.log(item.requestedAmount);
+									});
+								}
+								kendoGrid = $("#purchaseorder-sum-grid").data(
+										"kendoGrid");
+								var data = itemDataSource.data();
+								var total = 0;
+								var totalMoney = 0;
+								for (i = 0; i < data.length; i++) {
+									var item = data[i];
 
-					if(item.requestedAmount){
-						total = total + item.requestedAmount;
-					}
-					
-					if(item.referenceUnitPrice && item.requestedAmount){
-						totalMoney = totalMoney + item.requestedAmount * item.referenceUnitPrice;
-					}
-				}
-				sumDataSource.data({});
-				sumDataSource.add({
-					requestedNumbers : total,
-					requestedMoney: totalMoney
-				});
-				kendoGrid.setDataSource(sumDataSource);
-				
-			 }
+									if (item.requestedAmount) {
+										total = total + item.requestedAmount;
+									}
 
-		});
+									if (item.referenceUnitPrice
+											&& item.requestedAmount) {
+										totalMoney = totalMoney
+												+ item.requestedAmount
+												* item.referenceUnitPrice;
+									}
+								}
+								sumDataSource.data({});
+								sumDataSource.add({
+									requestedNumbers : total,
+									requestedMoney : totalMoney
+								});
+								kendoGrid.setDataSource(sumDataSource);
+
+								requestDataItem.totalMoney = totalMoney;
+								requestDataItem.requestedNumbers = total;
+							}
+
+						});
 	}
-
 
 }
