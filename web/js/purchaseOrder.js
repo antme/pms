1fde9c78ae74459245d3b5d0fe7c5c9ecdbdfd4b
dylan <chenlieping@gmail.com>
2013-06-06 11:18:16 +0800

    add comments

--- web/js/purchaseOrder.js
+++ web/js/purchaseOrder.js
@@ -1,34 +1,19 @@
+//抽象model对象， datasource对象必须绑定一个model为了方便解析parameterMap中需要提交的参数
 var model = kendo.data.Model.define({
-	id : "_id",
-	fields : {
-		orderId : {
-			editable : true
-		}
-	}
 });
-var dataSource = new kendo.data.DataSource({
+
+//外面列表页的datasource对象
+var orderDataSource = new kendo.data.DataSource({
 	transport : {
 		read : {
 			url : "/service/purcontract/order/list",
 			dataType : "jsonp"
 		},
-		update : {
-			url : "/service/purcontract/order/update",
-			dataType : "jsonp",
-			type : "post"
-		},
-		create : {
-			url : "/service/purcontract/order/add",
-			dataType : "jsonp",
-			type : "post"
-		},
-
 		destroy : {
 			url : "/service/purcontract/order/delete",
 			dataType : "jsonp",
 			type : "post"
 		},
-
 		parameterMap : function(options, operation) {
 			if (operation !== "read" && options.models) {
 				return {
@@ -39,17 +24,21 @@ var dataSource = new kendo.data.DataSource({
 	},
 	pageSize : 20,
 	batch : true,
+	
+	//必须绑定一个model为了方便解析parameterMap中需要提交的参数
 	schema : {
 		model : model
 	}
 });
 
 $(document).ready(function() {
-
+	
+	//初始化采购订单列表页
 	$("#grid").kendoGrid({
-		dataSource : dataSource,
+		dataSource : orderDataSource,
 		pageable : true,
-		editable : "popup",
+		
+		//自定义toolbar，参见html中模板代码
 		toolbar : [ {
 			template : kendo.template($("#template").html())
 		} ],
@@ -98,6 +87,7 @@ $(document).ready(function() {
 		{
 			command : [ {
 				text : "Edit",
+				//自定义点击事件
 				click : edit
 			}, {
 				name : "destroy",
@@ -111,9 +101,15 @@ $(document).ready(function() {
 	});
 });
 
+
+//声明一个总的对象用来传递数据
 var requestDataItem;
 
+
+//当window弹出的时候
 function onRequestSelectWindowActive(e) {
+	
+	//获取采购申请的数据，数据包含了成本清单
 	$("#purchaseRequest").kendoDropDownList({
 		dataTextField : "projectName",
 		dataValueField : "_id",
@@ -125,18 +121,21 @@ function onRequestSelectWindowActive(e) {
 				}
 			}
 		},
+		
+		//当用户选择不同的采购申请时候赋值给requestDataItem对象
 		select : function(e) {
 			requestDataItem = this.dataSource.at(e.item.index());
 		}
 	});
-
 }
 
+
+//窗口属性设置
 var reoptions = {
 	id : "purchaseorder-edit",
 	width : "1050px",
 	height : "600px",
-	title : "添加采购申请",
+	title : "采购订单编辑",
 	activate : onRequestSelectWindowActive
 };
 
@@ -159,42 +158,46 @@ var itemDataSource = new kendo.data.DataSource({
 		parameterMap : function(options, operation) {
 			if (operation !== "read" && options.models) {
 				return {
+					
+					//解析成json_p模式
 					json_p : kendo.stringify(requestDataItem)
 				};
 			}
 		},
 	},
-
 	schema : {
 		model : model
 	},
 	batch : true
-
 });
 
+
+//计算成本数据的datasouce
 var sumDataSource = new kendo.data.DataSource({
 
 });
 
 function showOrderWindow() {
+	
+	//如果用户用默认的采购申请，select event不会触发， 需要初始化数据
 	var kendoGrid = $("#purchaseRequest").data("kendoDropDownList");
 	if (!requestDataItem) {
 		requestDataItem = kendoGrid.dataSource.at(0);
 	}
-
+	
+	
+	
+	//新增，所以设置_id为空
 	requestDataItem.set("_id", "");
-	edit(null);
-
+	edit();
 }
 
 function submitOrder() {
-
-	var length = itemDataSource.data().length;
-
-	requestDataItem.set("changes", true);
-	console.log(requestDataItem);
+	//同步数据
 	itemDataSource.sync();
 }
+
+
 function sumOrders(e) {
 	var data = itemDataSource.data();
 	requestDataItem.orderList = data;
@@ -203,21 +206,27 @@ function sumOrders(e) {
 }
 
 function edit(e) {
+	
+	//初始化空对象
 	var dataItem = new model();
+	
+	if (requestDataItem) {
+		//如果是从采购申请选择过来的
+		dataItem = requestDataItem;
+	}
+	
 	if (e) {
+		//如果是从订单列表页点击edit过来的数据
 		e.preventDefault();
 		openWindow(reoptions);
 		dataItem = this.dataItem($(e.currentTarget).closest("tr"));
 		requestDataItem = dataItem;
 	}
 
-	if (requestDataItem) {
-		dataItem = requestDataItem;
-	}
 
-	kendo.bind($("#purchaseorder-edit"), dataItem);
+	//渲染成本编辑列表
 	itemDataSource.data(dataItem.orderList);
-
+	
 	$("#orderId").html(dataItem.orderId);
 	$("#projectName").html(dataItem.projectName);
 	$("#projectCode").html(dataItem.projectCode);
@@ -282,7 +291,7 @@ function edit(e) {
 			toolbar : [ "create" ],
 			editable : true,
 			scrollable : true,
-			width : "850px",
+			width : "950px",
 			edit : sumOrders
 
 		});
