d2825e3825c520d44f1416b08712b501371be125
shihua <shihua0715197@163.com>
2013-06-11 15:20:59 +0800

    add

---
+++ service/src/main/java/com/pms/service/mockbean/EqCostListBean.java
@@ -0,0 +1,17 @@
+package com.pms.service.mockbean;
+
+public class EqCostListBean {
+	public static final String EQ_LIST_SC_ID = "scId";
+
+	public static final String EQ_LIST_NO = "eqcostNo";//序号
+	public static final String EQ_LIST_MATERIAL_CODE = "eqcostMaterialCode";//物料代码
+	public static final String EQ_LIST_PRODUCT_NAME = "eqcostProductName";//产品名称
+	public static final String EQ_LIST_PRODUCT_TYPE = "eqcostProductType";  //规格型号
+	public static final String EQ_LIST_AMOUNT = "eqcostAmount";//数量
+	public static final String EQ_LIST_UNIT = "eqcostUnit";//单位
+	public static final String EQ_LIST_BRAND = "eqcostBrand";//品牌
+	public static final String EQ_LIST_BASE_PRICE = "eqcostBasePrice"; //成本价
+	public static final String EQ_LIST_TOTAL_AMOUNT = "eqcostTotalAmount";//小计
+	public static final String EQ_LIST_MEMO = "eqcostMemo";//备注
+	public static final String EQ_LIST_DISCOUNT_RATE = "eqcostDiscountRate"; //折扣率
+}
