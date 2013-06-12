f755468b34fc3be5c5cba44befb5e51ec9deaf03
chenlieping <lieping.chen@qingcheng.com>
2013-06-12 18:37:35 +0800

    purchase contract function

---
+++ service/src/main/java/com/pms/service/mockbean/PurchaseContract.java
@@ -0,0 +1,140 @@
+package com.pms.service.mockbean;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class PurchaseContract extends PurchaseRequestOrder{
+    
+    private String purchaseContractType;
+    
+    private String signBy;
+    
+    private String supplierName;
+    
+    private String supplierNameContact;
+    
+    private Date signDate;
+    
+    private BigDecimal firstPay;
+    
+    
+    private BigDecimal moneyProgress;
+    
+    private BigDecimal deposit;
+    
+
+    private String invoiceType;
+    
+    
+    private String description;
+
+
+    public String getPurchaseContractType() {
+        return purchaseContractType;
+    }
+
+
+    public void setPurchaseContractType(String purchaseContractType) {
+        this.purchaseContractType = purchaseContractType;
+    }
+
+
+    public String getSignBy() {
+        return signBy;
+    }
+
+
+    public void setSignBy(String signBy) {
+        this.signBy = signBy;
+    }
+
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+
+    public String getSupplierNameContact() {
+        return supplierNameContact;
+    }
+
+
+    public void setSupplierNameContact(String supplierNameContact) {
+        this.supplierNameContact = supplierNameContact;
+    }
+
+
+
+
+
+    public Date getSignDate() {
+        return signDate;
+    }
+
+
+    public void setSignDate(Date signDate) {
+        this.signDate = signDate;
+    }
+
+
+    public BigDecimal getFirstPay() {
+        return firstPay;
+    }
+
+
+    public void setFirstPay(BigDecimal firstPay) {
+        this.firstPay = firstPay;
+    }
+
+
+    public BigDecimal getMoneyProgress() {
+        return moneyProgress;
+    }
+
+
+    public void setMoneyProgress(BigDecimal moneyProgress) {
+        this.moneyProgress = moneyProgress;
+    }
+
+
+    public BigDecimal getDeposit() {
+        return deposit;
+    }
+
+
+    public void setDeposit(BigDecimal deposit) {
+        this.deposit = deposit;
+    }
+
+
+
+
+    public String getInvoiceType() {
+        return invoiceType;
+    }
+
+
+    public void setInvoiceType(String invoiceType) {
+        this.invoiceType = invoiceType;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    
+    
+    
+
+}
