b2d76407aef2df076f8bab652078fd62a3799a2f
Jack.Teng <jackteng2010@hotmail.com>
2013-06-19 19:53:08 +0800

    收票

---
+++ service/src/main/java/com/pms/service/mockbean/GetInvoiceBean.java
@@ -0,0 +1,11 @@
+package com.pms.service.mockbean;
+
+public class GetInvoiceBean {
+
+	public static final String getInvoiceCode = "getInvoiceCode";
+	public static final String getInvoiceType = "getInvoiceType";
+	public static final String getInvoiceDate = "getInvoiceDate";
+	
+	
+	
+}
