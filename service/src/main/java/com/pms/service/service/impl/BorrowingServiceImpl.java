e473239ba84e2c1f6c3e695820be0fb2d9140a63
huang kangning <huangkangning@gmail.com>
2013-06-19 17:55:41 +0800

    fix bug

--- service/src/main/java/com/pms/service/service/impl/BorrowingServiceImpl.java
+++ service/src/main/java/com/pms/service/service/impl/BorrowingServiceImpl.java
@@ -67,54 +67,77 @@ public class BorrowingServiceImpl extends AbstractService implements IBorrowingS
 		
 		List<String> pId = new ArrayList<String>();
 		for (Map<String, Object> p:list){
-			pId.add(p.get(BorrowingBean.BORROW_IN_PROJECT_ID).toString());
-			pId.add(p.get(BorrowingBean.BORROW_OUT_PROJECT_ID).toString());
-		}
-		
-		// 获取project信息
-		Map<String, Object> queryContract = new HashMap<String, Object>();
-		queryContract.put(ApiConstants.MONGO_ID, new DBQuery(DBQueryOpertion.IN, pId));
-		Map<String, Object> cInfoMap = dao.listToOneMapByKey(queryContract, DBBean.PROJECT, ApiConstants.MONGO_ID);
-		
-		List<String> pmId = new ArrayList<String>();
-		
-		for (Map<String, Object> p:list){
-			String inProjectId = p.get(BorrowingBean.BORROW_IN_PROJECT_ID).toString();
-			Map<String, Object> inProjectMap = (Map<String, Object>) cInfoMap.get(inProjectId);
-			if (inProjectMap != null) {
-				p.put(BorrowingBean.BORROW_IN_PROJECT_CODE, inProjectMap.get(ProjectBean.PROJECT_CODE));
-				p.put(BorrowingBean.BORROW_IN_PROJECT_NAME, inProjectMap.get(ProjectBean.PROJECT_NAME));
-				p.put(BorrowingBean.BORROW_IN_PROJECT_MANAGER, inProjectMap.get(ProjectBean.PROJECT_MANAGER));
-				pmId.add(inProjectMap.get(ProjectBean.PROJECT_MANAGER).toString());
+			if (p.containsKey(BorrowingBean.BORROW_IN_PROJECT_ID)) {
+				String inId = p.get(BorrowingBean.BORROW_IN_PROJECT_ID).toString();
+				if (!inId.equals("")) {
+					pId.add(inId.toString());
+				}
 			}
-			
-			String outProjectId = p.get(BorrowingBean.BORROW_OUT_PROJECT_ID).toString();
-			Map<String, Object> outProjectMap = (Map<String, Object>) cInfoMap.get(outProjectId);
-			if (outProjectMap != null) {
-				p.put(BorrowingBean.BORROW_OUT_PROJECT_CODE, outProjectMap.get(ProjectBean.PROJECT_CODE));
-				p.put(BorrowingBean.BORROW_OUT_PROJECT_NAME, outProjectMap.get(ProjectBean.PROJECT_NAME));
-				p.put(BorrowingBean.BORROW_OUT_PROJECT_MANAGER, outProjectMap.get(ProjectBean.PROJECT_MANAGER));
-				pmId.add(outProjectMap.get(ProjectBean.PROJECT_MANAGER).toString());
+			if (p.containsKey(BorrowingBean.BORROW_OUT_PROJECT_ID)) {
+				String outId = p.get(BorrowingBean.BORROW_OUT_PROJECT_ID).toString();
+				if (!outId.equals("")) {
+					pId.add(outId.toString());
+				}
 			}
 		}
 		
-		// 获取项目负责人信息
-		Map<String, Object> pmQueryContract = new HashMap<String, Object>();
-		pmQueryContract.put(ApiConstants.MONGO_ID, new DBQuery(DBQueryOpertion.IN, pmId));
-		pmQueryContract.put(ApiConstants.LIMIT_KEYS, new String[] {UserBean.USER_NAME});
-		Map<String, Object> pmMap = dao.listToOneMapByKey(pmQueryContract, DBBean.USER, ApiConstants.MONGO_ID);
-		
-		for (Map<String, Object> p:list){
-			String inPmId = p.get(BorrowingBean.BORROW_IN_PROJECT_MANAGER).toString();
-			Map<String, Object> inPmMap = (Map<String, Object>) pmMap.get(inPmId);
-			if (inPmMap != null) {
-				p.put(BorrowingBean.BORROW_IN_PROJECT_MANAGER, inPmMap.get(UserBean.USER_NAME));
+		// 有项目信息需要获取
+		if (!pId.isEmpty()) {
+			// 获取project信息
+			Map<String, Object> queryContract = new HashMap<String, Object>();
+			queryContract.put(ApiConstants.MONGO_ID, new DBQuery(DBQueryOpertion.IN, pId));
+			Map<String, Object> cInfoMap = dao.listToOneMapByKey(queryContract, DBBean.PROJECT, ApiConstants.MONGO_ID);
+			
+			List<String> pmId = new ArrayList<String>();
+			
+			for (Map<String, Object> p:list){
+				String inProjectId = p.get(BorrowingBean.BORROW_IN_PROJECT_ID).toString();
+				Map<String, Object> inProjectMap = (Map<String, Object>) cInfoMap.get(inProjectId);
+				if (inProjectMap != null) {
+					p.put(BorrowingBean.BORROW_IN_PROJECT_CODE, inProjectMap.get(ProjectBean.PROJECT_CODE));
+					p.put(BorrowingBean.BORROW_IN_PROJECT_NAME, inProjectMap.get(ProjectBean.PROJECT_NAME));
+					p.put(BorrowingBean.BORROW_IN_PROJECT_MANAGER, inProjectMap.get(ProjectBean.PROJECT_MANAGER));
+					pmId.add(inProjectMap.get(ProjectBean.PROJECT_MANAGER).toString());
+				}
+				
+				String outProjectId = p.get(BorrowingBean.BORROW_OUT_PROJECT_ID).toString();
+				Map<String, Object> outProjectMap = (Map<String, Object>) cInfoMap.get(outProjectId);
+				if (outProjectMap != null) {
+					p.put(BorrowingBean.BORROW_OUT_PROJECT_CODE, outProjectMap.get(ProjectBean.PROJECT_CODE));
+					p.put(BorrowingBean.BORROW_OUT_PROJECT_NAME, outProjectMap.get(ProjectBean.PROJECT_NAME));
+					p.put(BorrowingBean.BORROW_OUT_PROJECT_MANAGER, outProjectMap.get(ProjectBean.PROJECT_MANAGER));
+					pmId.add(outProjectMap.get(ProjectBean.PROJECT_MANAGER).toString());
+				}
 			}
 			
-			String outPmId = p.get(BorrowingBean.BORROW_OUT_PROJECT_MANAGER).toString();
-			Map<String, Object> outPmMap = (Map<String, Object>) pmMap.get(outPmId);
-			if (outPmMap != null) {
-				p.put(BorrowingBean.BORROW_OUT_PROJECT_MANAGER, outPmMap.get(UserBean.USER_NAME));
+			if (!pmId.isEmpty()) {
+				// 获取项目负责人信息
+				Map<String, Object> pmQueryContract = new HashMap<String, Object>();
+				pmQueryContract.put(ApiConstants.MONGO_ID, new DBQuery(DBQueryOpertion.IN, pmId));
+				pmQueryContract.put(ApiConstants.LIMIT_KEYS, new String[] {UserBean.USER_NAME});
+				Map<String, Object> pmMap = dao.listToOneMapByKey(pmQueryContract, DBBean.USER, ApiConstants.MONGO_ID);
+				
+				for (Map<String, Object> p:list){
+					if (p.containsKey(BorrowingBean.BORROW_IN_PROJECT_MANAGER)) {
+						String inPmId = p.get(BorrowingBean.BORROW_IN_PROJECT_MANAGER).toString();
+						if (!inPmId.equals("")) {
+							Map<String, Object> inPmMap = (Map<String, Object>) pmMap.get(inPmId);
+							if (inPmMap != null) {
+								p.put(BorrowingBean.BORROW_IN_PROJECT_MANAGER, inPmMap.get(UserBean.USER_NAME));
+							}
+						}
+					}
+					
+					if (p.containsKey(BorrowingBean.BORROW_OUT_PROJECT_MANAGER)) {
+						String outPmId = p.get(BorrowingBean.BORROW_OUT_PROJECT_MANAGER).toString();
+						if (!outPmId.equals("")) {
+							Map<String, Object> outPmMap = (Map<String, Object>) pmMap.get(outPmId);
+							if (outPmMap != null) {
+								p.put(BorrowingBean.BORROW_OUT_PROJECT_MANAGER, outPmMap.get(UserBean.USER_NAME));
+							}
+						}
+					}
+				}
 			}
 		}
 		
