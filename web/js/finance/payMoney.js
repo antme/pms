d0ea99a3c221761695225678c5a6479370555900
Jack.Teng <jackteng2010@hotmail.com>
2013-06-19 17:04:20 +0800

    付款

--- web/js/finance/payMoney.js
+++ web/js/finance/payMoney.js
@@ -15,24 +15,12 @@ $(document).ready(function () {
 			payDate:{
 				type:"date"
 			},
-			purchaseContractId:{
-				editable : false
-			},
-			purchaseContractCode:{
-				editable : false
-			},
-			supplierId:{
-				editable : false
-			},
-			supplierName:{
-				editable : false
-			},
-			supplierCardName:{
-				editable : false
-			},
-			supplierCardCode:{
-				editable : false
-			}
+			purchaseContractId:{},
+			purchaseContractCode:{},
+			supplierId:{},
+			supplierName:{},
+			supplierCardName:{},
+			supplierCardCode:{}
 		}
 	});
 
@@ -43,16 +31,6 @@ $(document).ready(function () {
 	            dataType: "jsonp",
 	            type : "post"
 	        },
-			update : {
-				url : baseUrl+"/paymoney/update",
-				dataType : "jsonp",
-				method : "post"
-			},
-			create : {
-				url : baseUrl+"/paymoney/add",
-				dataType : "jsonp",
-				method : "post"
-			},
 	        parameterMap: function(options, operation) {
 	        	if (operation !== "read" && options.models) {
 	                return {models: kendo.stringify(options.models)};
@@ -62,7 +40,8 @@ $(document).ready(function () {
 	    batch: true,
 	    pageSize: 10,
 	    schema: {
-	        model: requestModel
+	        model: requestModel,
+	        data:"data"
 	    }
 	});
     
@@ -73,66 +52,48 @@ $(document).ready(function () {
 	    toolbar: kendo.template($("#template").html()),
 	    columns: [
 	        {field:"payMoney", title:"付款金额"},
-	        {field:"payDate", title:"付款日期"},
-	        //{field: "purchaseContractCode", title:"采购合同编号"},
-	        //{field:"supplierName", title:"供应商"},
+	        {field:"payDate", title:"付款日期",format: "{0:yyyy/MM/dd hh:mm}"},
+	        {field:"purchaseContractCode", title:"采购合同编号"},
+	        {field:"supplierName", title:"供应商"},
 	        {field:"supplierCardName", title:"开户行"},
 	        {field:"supplierCardCode", title:"卡号"}
 	    ]
 	});
-	
-	
-    $("#addwindow").kendoWindow({
+    $("#popwindow").kendoWindow({
         width: "500px",
         title: "付款"
     });
-	$("#purchaseContractId").kendoDropDownList({
+	$("#showpopwindow").click(function(){
+		$("#popwindow").data("kendoWindow").open();
+	});
+	var currentObj = new requestModel();
+	kendo.bind($("#form-container"), currentObj);
+	
+	var pcdatasource = new kendo.data.DataSource({
+		transport : {
+			read : {
+				dataType : "jsonp",
+				url : baseUrl + "/listforselect/paymoney"
+			}
+		},
+	    schema: {data:"data"}
+	});	
+	
+	$("#selectpc").kendoDropDownList({
+		optionLabel: "请选择合同",
 		dataTextField : "purchaseContractCode",
 		dataValueField : "_id",
-		dataSource : {
-			transport : {
-				read : {
-					dataType : "jsonp",
-					url : baseUrl + "/listforselect/paymoney"
-				}
-			}
-		}
+		dataSource : pcdatasource,
 	});
-    
-	var cardNameList = ["招商银行","交通银行","中国银行","中国工商银行","中国建设银行","广发银行","中信银行",
-	                    "中国农业银行","平安银行","浦发银行","上海农商银行"];
-	
-	$("#supplierCardName").kendoAutoComplete({
-        dataSource: cardNameList,
-        placeholder: "选择银行"
-    });
-	 $("#payMoney").kendoNumericTextBox({
-		 min: 0,
-		 value:0
-	 });
-	$("#payDate").kendoDatePicker();
-	$("#addform").click(function(){
-		$("#addwindow").data("kendoWindow").open();
+	$("#payDate").kendoDateTimePicker({
+	    format: "yyyy/MM/dd hh:mm tt"
 	});
 	$("#submitform").click(function(){
-		var obj = new requestModel();
-		obj.payMoney = $("#payMoney").val();
-		obj.payDate = $("#payDate").val();
-		obj.purchaseContractId = $("#purchaseContractId").val();
-		obj.supplierCardName = $("#supplierCardName").val();
-		obj.supplierCardCode = $("#supplierCardCode").val();
-		listDatasource.add(obj);
-		listDatasource.sync();
-		var window =  $("#addwindow");
-
-		if (window.data("kendoWindow")) {
-			window.data("kendoWindow").close();
-		}
-		
-		var grid = $("#grid");
-		if (grid.data("kendoGrid")) {
-			grid.data("kendoGrid").refresh();
-		}		
+		currentObj.purchaseContractId=$("#selectpc").val();
+		postAjaxRequest(baseUrl+"/paymoney/add", {models:kendo.stringify(currentObj)}, saveSuccess);
 	});
+	function saveSuccess(){
+		location.reload();
+	}
 });
 
