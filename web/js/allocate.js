d8cf0046c7f131f4064d0a842b932ddb756111ba
huang kangning <huangkangning@gmail.com>
2013-06-28 11:32:20 +0800

    财务图表所需文件

--- web/js/allocate.js
+++ web/js/allocate.js
@@ -8,16 +8,31 @@ $(document).ready(function() {
         series: [{}]
     };
     
-    var url =  "../service/sc/get?_id=51b6ccd12b6001f9de9c5627";
+    var url =  "../service/sc/list";
     $.getJSON(url,  function(data) {
-    	var gotMoneyInfo = data.scGotMoneyInfo;
-    	var a = [];
-    	for ( var int = 0; int < gotMoneyInfo.length; int++) {
-    		var b = gotMoneyInfo[int].scGotMoney;
-    		a.push(parseInt(b));
-		}
+//    	var gotMoneyInfo = data.scGotMoneyInfo;
+//    	var a = [];
+//    	for ( var int = 0; int < gotMoneyInfo.length; int++) {
+//    		var b = gotMoneyInfo[int].scGotMoney;
+//    		a.push(parseInt(b));
+//		}
+    	var a = [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4];
     	console.log(a);
 		options.series[0].data = a;
+		options.xAxis.categories = [
+		                    'Jan',
+		                    'Feb',
+		                    'Mar',
+		                    'Apr',
+		                    'May',
+		                    'Jun',
+		                    'Jul',
+		                    'Aug',
+		                    'Sep',
+		                    'Oct',
+		                    'Nov',
+		                    'Dec'
+		                ];
 		var chart = new Highcharts.Chart(options);
     });
 });
