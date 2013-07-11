8b796b20c0b0ed2bf3f73dc85e516f432a4d5c9c
huangkn <huangkangnning@gmail.com>
2013-07-11 14:39:41 +0800

    到货列表过滤

--- service/src/main/java/com/pms/service/service/impl/ArrivalNoticeServiceImpl.java
+++ service/src/main/java/com/pms/service/service/impl/ArrivalNoticeServiceImpl.java
@@ -8,7 +8,6 @@ import java.util.Map;
 
 import com.pms.service.dbhelper.DBQuery;
 import com.pms.service.dbhelper.DBQueryOpertion;
-import com.pms.service.exception.ApiResponseException;
 import com.pms.service.mockbean.ApiConstants;
 import com.pms.service.mockbean.ArrivalNoticeBean;
 import com.pms.service.mockbean.CustomerBean;
@@ -23,7 +22,6 @@ import com.pms.service.service.IArrivalNoticeService;
 import com.pms.service.service.IPurchaseContractService;
 import com.pms.service.service.ISalesContractService;
 import com.pms.service.util.ApiUtil;
-import com.pms.service.util.status.ResponseCodeConstants;
 
 public class ArrivalNoticeServiceImpl extends AbstractService implements IArrivalNoticeService {
 	
@@ -54,6 +52,7 @@ public class ArrivalNoticeServiceImpl extends AbstractService implements IArriva
 
 	public Map<String, Object> list(Map<String, Object> params) {
 	    mergeMyTaskQuery(params, DBBean.ARRIVAL_NOTICE);
+	    mergeDataRoleQueryWithProject(params);
 		return dao.list(params, DBBean.ARRIVAL_NOTICE);
 	}
 
@@ -68,44 +67,7 @@ public class ArrivalNoticeServiceImpl extends AbstractService implements IArriva
 	}
 
 	public Map<String, Object> create(Map<String, Object> params) {
-		
-		String foreignKey = (String) params.get(ArrivalNoticeBean.FOREIGN_KEY);
-		String type = (String) params.get(ArrivalNoticeBean.SHIP_TYPE);
-		
-		if (dao.exist(ArrivalNoticeBean.FOREIGN_KEY, foreignKey, DBBean.ARRIVAL_NOTICE)) {
-			throw new ApiResponseException("对应到货通知已存在", ResponseCodeConstants.ARRIVAL_NOTICE_ALREADY_EXIST);
-		}
-		
-		if (!ArrivalNoticeBean.SHIP_TYPE_0.equals(type)) {
-			Map<String, Object> order = dao.findOne(ApiConstants.MONGO_ID, foreignKey, DBBean.PURCHASE_ORDER);
-			if (order != null) {
-				if (order.containsKey(PurchaseCommonBean.PROCESS_STATUS)) {
-					if (!PurchaseCommonBean.STATUS_ORDER_FINISHED.equals(order.get(PurchaseCommonBean.PROCESS_STATUS))) {
-						throw new ApiResponseException("采购未执行完毕", ResponseCodeConstants.PURCHASE_ORDER_UNFINISHED);
-					}
-				}
-				String eqcostDeliveryType = (String) order.get("eqcostDeliveryType");
-				
-				params.put(ArrivalNoticeBean.FOREIGN_CODE, order.get(PurchaseCommonBean.PURCHASE_ORDER_CODE));
-				params.put(ProjectBean.PROJECT_ID, order.get(PurchaseCommonBean.PROJECT_ID));
-				params.put(SalesContractBean.SC_ID, order.get(PurchaseCommonBean.SALES_COUNTRACT_ID));
-				if(!ApiUtil.isEmpty(type)){
-				    params.put(ArrivalNoticeBean.SHIP_TYPE, type); 
-				}else{
-				    params.put(ArrivalNoticeBean.SHIP_TYPE, eqcostDeliveryType);
-				}
-				// 入库
-				if (type == null) {
-					params.put(ArrivalNoticeBean.EQ_LIST, params.get(SalesContractBean.SC_EQ_LIST));
-				} else {
-					params.put(ArrivalNoticeBean.EQ_LIST, order.get(SalesContractBean.SC_EQ_LIST));
-				}
-				
-			}
-		}
-		
-		params.put(ArrivalNoticeBean.ARRIVAL_DATE, ApiUtil.formateDate(new Date(), "yyy-MM-dd"));
-		return dao.add(params, DBBean.ARRIVAL_NOTICE);
+		return null;
 	}
 	
 	public Map<String, Object> listProjectsForSelect(Map<String, Object> params){
